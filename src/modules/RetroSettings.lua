3198ed1c136b5bb3157c6b6db448c6eabdd978e1
Cathret <cathret@users.noreply.github.com>
2023-07-08 16:15:08 +0200

    Created RetroSettings module to be handle settings

---
+++ src/modules/RetroSettings.lua
@@ -0,0 +1,74 @@
+﻿---
+--- @module RetroSettings
+--- @description Submodule for RetroText to handle Localization-related stuff
+---
+--- @field CATEGORIES any All settings located in the Categories menu
+--- @field THEME any All settings located in the Theme menu
+--- @field AUDIO any All settings located in the Audio menu
+--- @field ARTWORK any All settings located in the Artwork menu
+--- @field SCAN any All settings located in the Scan Settings menu
+--- @field OTHER any All settings located in the Other Settings menu
+---
+local RetroSettings =
+{
+    -- TODO: Chosen Language is located in RetroText module for now
+
+    CATEGORIES =
+    {
+        startupCategory              = {}, 
+        enableHomebrewCategory       = true,
+        enableRecentlyPlayedCategory = true,
+        enableDisplayAllCategory     = true,
+        showHiddenGames              = false,
+        showCollections              = true
+    },
+    
+    THEME =
+    {
+        themeColor             = {},
+        enableReflectionEffet  = true,
+        defaultBackgroundName  = "",
+        enableSmoothScrolling  = true,
+        enable2dView           = true,
+        enableChangeViews      = true
+    },
+    
+    AUDIO =
+    {
+        enableSounds = true,
+        enableMusic = true,
+        shuffleMusic = true,
+        enableCustomMusic = true -- TODO: new feature to implement, permit to set and use custom music for each game when hovering
+    },
+    
+    ARTWORK =
+    {
+        enableDownloadCover = {},
+        enableDownloadBackground = {},
+        enableGameDetailsBackground = true,
+        enableCustomBackground = true, -- TODO: new feature to implement, use game background even if not in Details menu
+    },
+    
+    SCAN =
+    {
+        GAME_DIRECTORIES =
+        {
+            
+        },
+        
+        enableStartupScan = false,
+        adrenalineGamesToScanFolder = ""
+    },
+    
+    OTHER =
+    {
+        enableSwapXandO = false, -- Base setting is X to confirm
+        adrenalinePsButtonBehaviour = {},
+        enableShowMissingCovers = true,
+        
+        CUSTOM_COLLECTIONS =
+        {
+            
+        }
+    }
+}
