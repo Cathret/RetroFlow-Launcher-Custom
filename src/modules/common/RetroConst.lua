a5d2e8266538e263539934beb0fa04f7760929ef
Cathret <cathret@users.noreply.github.com>
2023-07-08 17:19:30 +0200

    Created RetroConst file to be able to keep information on common Const information about RetroFlow.
    For now, contains the App/Data directory paths, the AppVersion, but also all the different useful paths for Roms/Covers/Backgrounds/Icons.
    Also contains the folder names by ConsoleKeys.

---
+++ src/modules/common/RetroConst.lua
@@ -0,0 +1,35 @@
+﻿---
+--- @module RetroConst
+--- @description Submodule for all RetroFlow modules to handle const and common information
+---
+--- @field RomsMainDir string Path to the main ROMS directory
+--- @field CoversDir string Path to the COVERS directory
+--- @field BackgroundDir string Path to the BACKGROUND directory
+--- @field IconsDir string Path to the ICONS directory
+--- @field ConsoleFolderNames any Map<ConsoleKey, String> Name of all the Console folders
+---
+local RetroConst =
+{
+    RomsMainDir        = nil,
+    CoversDir          = nil,
+    BackgroundsDir     = nil,
+    IconsDir           = nil,
+    ConsoleFolderNames = nil,
+}
+
+local INVARIABLE_CONSTANTS =
+{
+    AppDirectory  = "app0:", -- Equivalent to "ux0:/app/RETROFLOW/"
+    DataDirectory = "ux0:/data/RetroFlow/",
+    AppVersion    = "0.2"
+}
+
+RetroConst.RomsMainDir = INVARIABLE_CONSTANTS.DataDirectory .. "ROMS/"
+RetroConst.CoversDir = INVARIABLE_CONSTANTS.DataDirectory .. "COVERS/"
+RetroConst.BackgroundsDir = INVARIABLE_CONSTANTS.DataDirectory .. "BACKGROUNDS/"
+RetroConst.IconsDir = INVARIABLE_CONSTANTS.DataDirectory .. "ICONS/"
+
+-- We retrieve only relative path, so we need to add DataDirectory to values
+RetroConst.ConsoleFolderNames = require("modules/common/const/RetroConst_RomFolders.lua")
+
+return RetroConst
